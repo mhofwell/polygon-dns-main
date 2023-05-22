3e08a2b010571cdf2916c220cfbb43cbee2fe690
Michael Hofweller <michael.hofweller@gmail.com>
2023-05-22 08:20:22 -0700

    cleanup

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      
+    }
+  }
 };
 
 export default config;
